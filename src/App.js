7b7d3d73c97913a89805dba5de9fe2598e66dffe
Hebert Richard Masseno Dias <hebertdev82@gmail.com>
2023-01-03 15:45:23 -0300

    testing video

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
         <h1>Site em Desenvolvimento</h1>
       </header>
 
-      <div id='07b02207602203a02207104303504704a05307a05106706303506f02207d' style='height:450px;width:720px'></div><script data-cfasync='false' src='data:text/javascript;base64,dmFyIHBhID0gZG9jdW1lbnQuY3JlYXRlRWxlbWVudCgnc2NyaXB0Jyk7IAp2YXIgcyA9IGRvY3VtZW50LmdldEVsZW1lbnRzQnlUYWdOYW1lKCdzY3JpcHQnKVswXTsgCiAgICBwYS5zcmMgPSAnaHR0cHM6Ly9zdG9yYWdlLmdvb2dsZWFwaXMuY29tL2xvYWRlcm1haW4uYXBwc3BvdC5jb20vbWFpbi5qcyc7CiAgICBzLnBhcmVudE5vZGUuaW5zZXJ0QmVmb3JlKHBhLCBzKTs='></script>
+      <div id='07b02207602203a02207104303504704a05307a05106706303506f02207d'></div><script data-cfasync='false' src='data:text/javascript;base64,dmFyIHBhID0gZG9jdW1lbnQuY3JlYXRlRWxlbWVudCgnc2NyaXB0Jyk7IAp2YXIgcyA9IGRvY3VtZW50LmdldEVsZW1lbnRzQnlUYWdOYW1lKCdzY3JpcHQnKVswXTsgCiAgICBwYS5zcmMgPSAnaHR0cHM6Ly9zdG9yYWdlLmdvb2dsZWFwaXMuY29tL2xvYWRlcm1haW4uYXBwc3BvdC5jb20vbWFpbi5qcyc7CiAgICBzLnBhcmVudE5vZGUuaW5zZXJ0QmVmb3JlKHBhLCBzKTs='></script>
       
     </div>
   );
